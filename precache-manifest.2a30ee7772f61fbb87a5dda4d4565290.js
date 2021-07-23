3c12c81f56a2e9bdbc92543f3cfe04038e455adc
Parsath <bilel.taktak@esprit.tn>
2021-07-23 13:11:47 +0200

    deploy 2

---
+++ precache-manifest.2a30ee7772f61fbb87a5dda4d4565290.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1534ccd4b235deb5fcda",
+    "url": "/prodigy/css/index.3f1b1008.css"
+  },
+  {
+    "revision": "7311649a6b07d67810c3d9a18b9286d9",
+    "url": "/prodigy/img/Logo-Prodigy-V-Cheap-Black-Rev.7311649a.png"
+  },
+  {
+    "revision": "a058f6ec59d1078242ca8898fc9b8daa",
+    "url": "/prodigy/img/click.a058f6ec.svg"
+  },
+  {
+    "revision": "020054470a56d76ff09c6e94bcbd9912",
+    "url": "/prodigy/img/code.02005447.svg"
+  },
+  {
+    "revision": "cf3c99a1934b1759c1a8a8912390b1fb",
+    "url": "/prodigy/img/team.cf3c99a1.svg"
+  },
+  {
+    "revision": "43d9f23b77901bb6a20ee5b8b2357c23",
+    "url": "/prodigy/img/uncle_sam_1366.43d9f23b.png"
+  },
+  {
+    "revision": "accbed67fa2706b324a917db9d75edf0",
+    "url": "/prodigy/img/uncle_sam_1536.accbed67.png"
+  },
+  {
+    "revision": "a3682c6c6de2f7467cdb3722e26afc29",
+    "url": "/prodigy/img/uncle_sam_v2.a3682c6c.png"
+  },
+  {
+    "revision": "4abb34bd37df90cc10908e20e72fca1c",
+    "url": "/prodigy/index.html"
+  },
+  {
+    "revision": "ac8c94fff385d9b196f0",
+    "url": "/prodigy/js/chunk-vendors.85754192.js"
+  },
+  {
+    "revision": "1534ccd4b235deb5fcda",
+    "url": "/prodigy/js/index.92d08bed.js"
+  },
+  {
+    "revision": "e01e92ef9d4f3a461bd4114feaa87dd4",
+    "url": "/prodigy/manifest.json"
+  },
+  {
+    "revision": "c5047644027b59a19fae259b3fe6363c",
+    "url": "/prodigy/media/simplify-1366_768.c5047644.mp4"
+  },
+  {
+    "revision": "29492f568514b19ceddb6c96dcc2308c",
+    "url": "/prodigy/media/simplify-1440_900.29492f56.mp4"
+  },
+  {
+    "revision": "7e21ce35359afd664fa3f80e316e994c",
+    "url": "/prodigy/media/simplify-1536_864.7e21ce35.mp4"
+  },
+  {
+    "revision": "4972a0d925a397530adaa9e4e8e64371",
+    "url": "/prodigy/media/simplify-1920_1080.4972a0d9.mp4"
+  },
+  {
+    "revision": "6021ef884b91fba14928f44d6094fa79",
+    "url": "/prodigy/media/simplify-360_780.6021ef88.mp4"
+  },
+  {
+    "revision": "af3bf5a07dde73e54a98b20469a761e8",
+    "url": "/prodigy/media/simplify-375_812.af3bf5a0.mp4"
+  },
+  {
+    "revision": "f3e8bf8a3bae1fec73d90cc63668e482",
+    "url": "/prodigy/media/simplify-414_896.f3e8bf8a.mp4"
+  }
+]);
